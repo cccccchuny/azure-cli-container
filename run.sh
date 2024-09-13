cab61a27f536d02b8b7fcf9e8a07f852ff9815e9
cccccchuny <cccccchuny@gmail.com>
2024-09-13 12:46:18 +0900

    20240913 third commit deploy 생성시 다운되는 문제 해결 (sleep inpinity)

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
-docker run -itd --name az-cli azure-cli-container:1 bash
+### local container
+# docker run -itd --name az-cli azure-cli-container:1 bash
+
+### k8s deploy
+kubectl apply -f secret.yaml
+kubectl apply -f deploy.yaml
+
 
